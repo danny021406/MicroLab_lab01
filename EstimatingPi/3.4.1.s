d62ae0462d300f050abc76e2a4e09ee69f8b8946
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-10-02 01:09:19 +0800

    3.4.1 floating point basic ok but vldr function cannot work

--- EstimatingPi/3.4.1.s
+++ EstimatingPi/3.4.1.s
@@ -14,13 +14,13 @@ enable_fpu:
 
  
 						// CPACR is located at address 0xE000ED88
-	ldr.w R0, =0xE000ED88
+	ldr.w r0, =0xE000ED88
 						//Read CPACR
-	ldr R1, [R0]
+	ldr r1, [R0]
 						//Set bits 20-23 to enable CP10 and CP11 coprocessors
-	orr R1, R1, #(0xF << 20)
+	orr r1, r1, #(0xF << 20)
 						//Write back the modified value to the CPACR
-	str R1, [R0]		// wait for store to complete
+	str r1, [r0]		// wait for store to complete
 	dsb
 						//reset pipeline now the FPU is enabled
 	isb
@@ -39,13 +39,21 @@ main:
 	//and show the register result in your report 
  
 	// s2=x-y   
+	vsub.f32 s2, s0, s1
 	// s2=x*y  
+	vmul.f32 s2, s0, s1
 	// s2=x/y 
- 
+	vdiv.f32 s2, s0, s1
 	 // load z into r0,  
+	ldr r0,=z  
+	
 	 // copy z from r0 to s2,  
+	 vldr.f32 s2, [r0]
 	 // convert z from U32 to float representation F32 in s2  
+	 vstr.f32 s2, [r0]
 	 // calculate s3=z+x+y 
+	 vadd.f32 s4, s0, s1
+	 vadd.f32 s3, s4, s2
  L: b L
  
  
