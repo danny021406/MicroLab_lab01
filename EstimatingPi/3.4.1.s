cecf61798ab691ce88fff71212d06df399ab6e93
danny <dammy021406@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnn>
2017-10-02 13:19:34 +0800

    3.4.1 vldr fixed becauce of not converting integer to floating

--- EstimatingPi/3.4.1.s
+++ EstimatingPi/3.4.1.s
@@ -1,18 +1,18 @@
 
- .syntax unified  
- .cpu cortex-m4  
- .thumb 
- 
-.data  
-	x: .float 0.123  
-	y: .float 0.456  
-	z: .word 20 
-.text  
-	.global main 
- 
-enable_fpu:  
+ .syntax unified
+ .cpu cortex-m4
+ .thumb
 
- 
+.data
+	x: .float 0.123
+	y: .float 0.456
+	z: .word 20
+.text
+	.global main
+
+enable_fpu:
+
+
 						// CPACR is located at address 0xE000ED88
 	ldr.w r0, =0xE000ED88
 						//Read CPACR
@@ -24,36 +24,35 @@ enable_fpu:
 	dsb
 						//reset pipeline now the FPU is enabled
 	isb
- 
-	bx lr 
- 
-main:  
-	bl enable_fpu  
-	ldr r0,=x  
-	vldr.f32 s0,[r0]  
+
+	bx lr
+
+main:
+	bl enable_fpu
+	ldr r0,=x
+	vldr.f32 s0,[r0]
 	ldr r0,=y
-	vldr s1,[r0]  
-	vadd.f32 s2,s0,s1  
-	// Your code start from here  
-	//Calculate the following values using FPU instructions  
-	//and show the register result in your report 
- 
-	// s2=x-y   
+	vldr s1,[r0]
+	vadd.f32 s2,s0,s1
+	// Your code start from here
+	//Calculate the following values using FPU instructions
+	//and show the register result in your report
+
+	// s2=x-y
 	vsub.f32 s2, s0, s1
-	// s2=x*y  
+	// s2=x*y
 	vmul.f32 s2, s0, s1
-	// s2=x/y 
+	// s2=x/y
 	vdiv.f32 s2, s0, s1
-	 // load z into r0,  
-	ldr r0,=z  
-	
-	 // copy z from r0 to s2,  
+	 // load z into r0,
+	ldr r0,=z
+
+	 // copy z from r0 to s2,
 	 vldr.f32 s2, [r0]
-	 // convert z from U32 to float representation F32 in s2  
-	 vstr.f32 s2, [r0]
-	 // calculate s3=z+x+y 
+	 // convert z from U32 to float representation F32 in s2
+	 vcvt.f32.s32 s2, s2
+	 // calculate s3=z+x+y
 	 vadd.f32 s4, s0, s1
 	 vadd.f32 s3, s4, s2
  L: b L
- 
- 
+
