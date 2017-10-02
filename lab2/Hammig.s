742c5a3e294a4be3ab120f1d27b6bc567533df82
EarlessCat <e33624303@gmail.com>
2017-10-03 00:24:50 +0800

    hammig.s all done

--- lab2/Hammig.s
+++ lab2/Hammig.s
@@ -11,16 +11,24 @@
 
 hamm:
 	eors r5, r3, r4
-	str r5, [r2]
+    movs r6, #0
 	bx lr
-
+dis:
+    cmp r5, #0
+    beq end
+    sub r7, r5, #1
+    and r5, r5, r7
+    add r6, r6, #1
+    b dis
 main:
-	ldr r0, =X 
+	ldr r0, =X
 	ldr r1, =Y
 	ldr r3, [r0]
 	ldr r4, [r1]
 	
 	ldr r2, =result
 	bl hamm
-
+    b dis
+end:
+    str r6, [r2]
 L: b L
