4d7de90d4c8bea46f9d59eac526ae8e8d6e9131d
EarlessCat <e33624303@gmail.com>
2017-10-04 15:17:37 +0800

    modify the range of bubblesort.s

--- lab2/BubbleSort.s
+++ lab2/BubbleSort.s
@@ -1,25 +1,25 @@
  .syntax unified
  .cpu cortex-m4
- .thumb 
- 
+ .thumb
+
 
 .data
 	arr1: .byte 0x19, 0x34, 0x14, 0x32, 0x52, 0x23, 0x61, 0x29
-	arr2: .byte 0x18, 0x17, 0x33, 0x16, 0xFA, 0x20, 0x55, 0xAC 
+	arr2: .byte 0x18, 0x17, 0x33, 0x16, 0xFA, 0x20, 0x55, 0xAC
 
-.text     
-	.global main 
+.text
+	.global main
 	.equ len, 8
 	.equ oneElement, 1
-	
-	
-do_sort:    
+
+
+do_sort:
 	movs r2, #0
 	subs r2, r2, #1
 	movs r7, #len
-	
-	
-outLoop:	
+	subs r7, r7, #1
+
+outLoop:
 	adds r2, r2, #1
 	cmp r2, r7
 	beq end
@@ -28,34 +28,34 @@ outLoop:
 	movs r8, r0
 	subs r8, #1
 	subs r6, r7, r2
-	
+
 innerLoop:
 	adds r3, r3, #1
 	adds r8, r8, #1
-	
+
 	cmp r3, r6
 	beq outLoop
-	
+
 	ldrb r4, [r8]
 	ldrb r5, [r8, #1]
-	
+
 	cmp r4, r5
 	ble innerLoop
-	
+
 	strb r5, [r8]
 	strb r4, [r8, #1]
-	
+
 	b innerLoop
 
 end:
-	bx lr 
+	bx lr
 main:
-	ldr r0, =arr1     
-	bl do_sort     
-	ldr r0, =arr2     
-	bl do_sort 
+	ldr r0, =arr1
+	bl do_sort
+	ldr r0, =arr2
+	bl do_sort
 
-L: b L 
+L: b L
 
 /*
 
@@ -64,7 +64,7 @@ int bubbleSort(int a[], int len)
 	int i, j, temp;
 	for (i = 0; i < len - 1; i++)
 		for (j = 0; j < len - 1 - i; j++)
-			if (a[j] > a[j + 1]) 
+			if (a[j] > a[j + 1])
                 temp = a[j];
                 a[j] = a[j + 1];
                 a[j + 1] = temp;
