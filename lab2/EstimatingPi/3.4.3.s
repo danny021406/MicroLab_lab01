1f76a03fb787a0f36d96f907e3dd6664a9213828
EarlessCat <e33624303@gmail.com>
2017-10-05 10:47:33 +0800

    making change on 3.4.3.s

--- lab2/EstimatingPi/3.4.3.s
+++ lab2/EstimatingPi/3.4.3.s
@@ -1,35 +1,35 @@
- .syntax unified  
- .cpu cortex-m4  
- .thumb 
- .text  
- .global main  
- .equ RCC_BASE,0x40021000  
- .equ RCC_CR,0x0  
- .equ RCC_CFGR,0x08  
- .equ RCC_PLLCFGR,0x0c  
- .equ RCC_CCIPR,0x88  
- .equ RCC_AHB2ENR,0x4C  
- .equ RNG_CLK_EN,18 
- 
- // Register address for RNG (Random Number Generator)  
- .equ RNG_BASE,0x50060800 //RNG BASE Address  
- .equ RNG_CR_OFFSET,0x00 //RNG Control Register  
- .equ RNGEN,2 // RNG_CR bit 2 
- 
- .equ RNG_SR_OFFSET,0x04 //RNG Status Register  
- .equ DRDY,0 // RNG_SR bit 0  
- .equ RNG_DR_OFFSET,0x08 //RNG Data Register (Generated random number!)  
- //Data Settings for 3.4.4 
- 
-  .equ SAMPLE,1000000 
- 
-SetFlag:  
-	ldr r2,[r0,r1]  
-	orr r2,r2,r3  
-	str r2,[r0,r1]  
-	bx lr 
- 
-EnableFpu:  
+ .syntax unified
+ .cpu cortex-m4
+ .thumb
+ .data
+  	x: .float 4294967296.0
+ .text
+ .global main
+ .equ RCC_BASE,0x40021000
+ .equ RCC_CR,0x0
+ .equ RCC_CFGR,0x08
+ .equ RCC_PLLCFGR,0x0c
+ .equ RCC_CCIPR,0x88
+ .equ RCC_AHB2ENR,0x4C
+ .equ RNG_CLK_EN,18
+ // Register address for RNG (Random Number Generator)
+ .equ RNG_BASE,0x50060800 //RNG BASE Address
+ .equ RNG_CR_OFFSET,0x00 //RNG Control Register
+ .equ RNGEN,2 // RNG_CR bit 2
+
+ .equ RNG_SR_OFFSET,0x04 //RNG Status Register
+ .equ DRDY,0 // RNG_SR bit 0
+ .equ RNG_DR_OFFSET,0x08 //RNG Data Register (Generated random number!)
+ //Data Settings for 3.4.4
+.equ SAMPLE,1000000
+
+SetFlag:
+	ldr r2,[r0,r1]
+	orr r2,r2,r3
+	str r2,[r0,r1]
+	bx lr
+
+EnableFpu:
 
 
 						// CPACR is located at address 0xE000ED88
@@ -45,83 +45,109 @@ EnableFpu:
 	isb
 
 	bx lr
- 
-EnableRng:  
-	//Your code start from here  
-	//Set the RNGEN bit to 1    
-	ldr r1, =RNG_CR_OFFSET
-	movs r4, #4
-	str r4, [r1]
-	bx lr 
- 
-GetRand:  
-	//Your code start from here  
-	//read RNG_SR  
-	//check DRDY bit, wait until to 1  
-	//read RNG_DR for random number and store into a register for later usage 
+
+EnableRng:
+	//Your code start from here
+	//Set the RNGEN bit to 1
 	ldr r4, =RNG_SR_OFFSET
-	ldr r5, [r4]
+	ldr r0, =RNG_BASE
+	ldr r5, [r0,r4]
+	ldr r1, =RNG_CR_OFFSET
+	ldr r11, [r0,r1]
+	add r11, r11, #4
+	str r11, [r0,r1]
+	bx lr
+
+GetRand:
+	//Your code start from here
+	//read RNG_SR
+	//check DRDY bit, wait until to 1
+	//read RNG_DR for random number and store into a register for later usage
+	ldr r4, =RNG_SR_OFFSET
+	ldr r0, =RNG_BASE
+	ldr r5, [r0,r4]
 	cmp r5, DRDY
 	beq GetRand
 	ldr r4, =RNG_DR_OFFSET
-	ldr r5, [r4]
-	bx lr 
- 
-main: 
-//RCC Settings  
-	ldr r0,=RCC_BASE  
-	ldr r1,=RCC_CR  
-	ldr r3,=#(1<<8) //HSION  
-	bl SetFlag  
-	ldr r1,=RCC_CFGR  
-	ldr r3,=#(3<<24) //HSI16 selected  
-	bl SetFlag  
-	ldr r1,=RCC_PLLCFGR  
-	ldr r3,=#(1<<24|1<<20|1<<16|10<<8|2<<0)  
-	bl SetFlag  
-	ldr r1,=RCC_CCIPR  
-	ldr r3,=#(2<<26)  
-	bl SetFlag  
-	ldr r1,=RCC_AHB2ENR  
-	ldr r3,=#(1<<RNG_CLK_EN)  
-	bl SetFlag  
-	ldr r1,=RCC_CR  
-	ldr r3,=#(1<<24) //PLLON  
-	bl SetFlag 
-chk_PLLON:  
+	ldr r5, [r0,r4]
+	bx lr
+
+main:
+//RCC Settings
+	ldr r0,=RCC_BASE
+	ldr r1,=RCC_CR
+	ldr r3,=#(1<<8) //HSION
+	bl SetFlag
+	ldr r1,=RCC_CFGR
+	ldr r3,=#(3<<24) //HSI16 selected
+	bl SetFlag
+	ldr r1,=RCC_PLLCFGR
+	ldr r3,=#(1<<24|1<<20|1<<16|10<<8|2<<0)
+	bl SetFlag
+	ldr r1,=RCC_CCIPR
+	ldr r3,=#(2<<26)
+	bl SetFlag
+	ldr r1,=RCC_AHB2ENR
+	ldr r3,=#(1<<RNG_CLK_EN)
+	bl SetFlag
+	ldr r1,=RCC_CR
+	ldr r3,=#(1<<24) //PLLON
+	bl SetFlag
+chk_PLLON:
 	ldr r2,[r0,r1]
 	ands r2,r2,#(1<<25)
 	beq chk_PLLON
-  
-//Your code start from here 
-//Enable FPU,RNG 
-//Generate 2 random U32 number x,y 
-//Map x,y in unit range [0,1] using FPU 
-//Calculate the z=sqrt(x^2+y^2) using FPU 
+
+//Your code start from here
+//Enable FPU,RNG
+//Generate 2 random U32 number x,y
+//Map x,y in unit range [0,1] using FPU
+//Calculate the z=sqrt(x^2+y^2) using FPU
 //Show the result of z in your report
-	
-	bl EnableFpu  
-	bl EnableRng  
-	
-	movs r6, #1000000
-	vmovs s4, #1000000
+
+	bl EnableFpu
+	bl EnableRng
+
+	movs r6, #1000
+	muls r6, r6, r6
+	vmov.f32 s4, r6
+	vcvt.f32.u32 s4, s4
+
 	movs r8, #1
-	vmov s3, #1
-	
+	vmov.f32 s5, r8
+	vcvt.f32.u32 s5, s5
+
+
+	ldr r12, =x
+	vldr s3, [r12]
+
 StartLoop:
 	cmp r6, #0
 	ble EndLoop
 	subs r6, r6, #1
+
 	bl GetRand
+	cmp r5, #0
+	ble GetPos1
+back1:
 	vmov.f32 s0, r5
 	vcvt.f32.u32 s0, s0
-	
+	vdiv.f32  s0, s0, s3
+
+	bl GetRand
+	cmp r5, #0
+	ble GetPos2
+back2:
+	vmov.f32 s1, r5
+	vcvt.f32.u32 s1, s1
+	vdiv.f32  s1, s1, s3
+
 	vmul.f32 s0, s0, s0
 	vmul.f32 s1, s1, s1
 	vadd.f32 s1, s0, s1
-	
+
 	vsqrt.f32 s2, s1
-	vcmp s3, s2
+	vcmp.f32 s5, s2
 	ble StartLoop
 	adds r8, r8, #1
 	b StartLoop
@@ -129,11 +155,19 @@ StartLoop:
 EndLoop:
 	vmov.f32 s0, r8
 	vcvt.f32.u32 s0, s0
-	vdiv s4, s4, s0
-	vmul s4, s4, #4
-	
+	vdiv.f32 s4, s0, s4
+	vmov.f32 s6, #4.0
+	vmul.f32 s4, s4, s6
 
-L:b L	
+
+L:b L
+
+GetPos1:
+	neg r5, r5
+	b back1
+GetPos2:
+	neg r5, r5
+	b back2
 
 
 
